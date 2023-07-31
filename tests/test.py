1dfd81e9ed349b7e88be4bc5bede2ac279b4fda4
Vít Novotný <witiko@mail.muni.cz>
2023-07-31 13:13:22 +0200

    Draft CLI of `test.py`

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python3
 
 """Run a test for each testfile passed as an argument."""
+
+from typing import List
+
+import click
+
+
+UPDATE_TESTS = False
+FAIL_FAST = False
+
+
+@click.command()
+@click.argument('testfiles',
+                nargs=-1,
+                type=click.Path(exists=True, dir_okay=False, file_okay=True),
+                required=True)
+@click.option('--update-tests/--no-update-tests',
+              help='Update testfiles with unexpected results',
+              default=UPDATE_TESTS)
+@click.option('--fail-fast/--no-fail-fast',
+              help='When a test fails, stop immediately',
+              default=FAIL_FAST)
+def main(testfiles: List[str], update_tests: bool, fail_fast: bool):
+    pass
+
+
+if __name__ == '__main__':
+    main()
