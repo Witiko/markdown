cb0db531d75dfc6daeffa03a7fa6ae00ad78101a
Vít Novotný <witiko@mail.muni.cz>
2023-08-16 11:10:22 +0200

    Revert "Test the memory leak"
    This reverts commit fdbb18be87b2eb6688c3e966b90322e1d273a7c9.

--- memory-leak.lua
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env texlua
-local ran_ok, kpse = pcall(require, "kpse")
-if ran_ok then kpse.set_program_name("luatex") end
-local markdown = require("markdown")
-
-for _ = 1, 100 do
-  markdown.new({singletonCache = false})("Hello *world*!")
-end
